7869ee0f93ebeb3d0554acb4742f21e98eb92786
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-11-26 19:28:20 -0600

    prime numbers

--- rsa.sh
+++ rsa.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
-# Factorize as many numbers as possible into a product of two smaller numbers.
+# Factorize numbers into a product of two smaller numbers
 
 while IFS= read -r LINE
 do
-    let FLAG=1
-    let DIV=2
-    while [ $FLAG -eq 1 ]
-    do
-	REST=$(($LINE%$DIV))
-	if [[ $REST -eq 0 ]]
-        then
-            let NUM=$LINE
-            let COUNT=$(($NUM/$DIV))
-            echo "$LINE=$COUNT*$DIV"
-            let FLAG=0
-        fi
-        let DIV=$(($DIV+1))
-    done
-done < $1
+	NUM=$LINE
+	for ((DIV = 2; DIV * DIV <= NUM; DIV++))
+	do
+		while ((NUM % DIV == 0))
+		do
+			printf "%d=%d*%d\n" "$LINE" "$DIV" "$((NUM / DIV))"
+			((NUM /= DIV))
+		done
+	done
+	if ((NUM > 1)); then
+		printf "%d=%d*%d\n" "$LINE" "$NUM" 1
+	fi
+done < "$1"
